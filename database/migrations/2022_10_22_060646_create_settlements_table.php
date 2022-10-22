a19deb4b00264b6c64f2694103882f68b8c491fc
Matej Malicky <matej.malicky@gmail.com>
2022-10-22 08:57:38 +0200

    feat: create settlementi model and migration

---
+++ database/migrations/2022_10_22_060646_create_settlements_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settlements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index();
+            $table->string('mayor_name');
+            $table->string('city_hall_address');
+            $table->string('phone');
+            $table->string('fax')->nullable();
+            $table->string('email')->nullable();
+            $table->string('web_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settlements');
+    }
+};
