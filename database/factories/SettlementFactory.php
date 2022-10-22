b70988c6b08319038f40105b48a6971f4bbaaabe
Matej Malicky <matej.malicky@gmail.com>
2022-10-22 09:26:04 +0200

    feat: add factory for Settlements

---
+++ database/factories/SettlementFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Settlement;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Settlement>
+ */
+class SettlementFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $settlements = Settlement::all();
+
+        return [
+            'parent_id' => $settlements->count() > 0 ? $this->faker->randomElement($settlements)['id'] : null,
+            'name' => $this->faker->city(),
+            'mayor_name' => $this->faker->name(),
+            'city_hall_address' => $this->faker->address(),
+            'phone' => $this->faker->phoneNumber(),
+            'fax' => $this->faker->optional()->email(),
+            'email' => $this->faker->email(),
+            'web_address' => $this->faker->url(),
+        ];
+    }
+}
