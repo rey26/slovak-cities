6413ad9b640cb9bbbcf1b1dace82ae53aa3d6eec
Matej Malicky <matej.malicky@gmail.com>
2022-10-25 18:25:56 +0200

    feat: add footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,92 @@
+<div class="container">
+    <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+            <div class="col-6 col-md">
+                <h6 class="text-uppercase">Adresa a kontakt</h6>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">ŠÚKL</a></li>
+                    <li><a class="text-muted" href="#">Kvetná 11</a></li>
+                    <li><a class="text-muted" href="#">825 08 Bratislava 26</a></li>
+                    <li><a class="text-muted" href="#">Ústredňa:</a></li>
+                    <li><a class="text-muted" href="#">+421-2-50701 111</a></li>
+                </ul>
+                <h6 class="text-uppercase">Kontakty</h6>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">tefefónne čisla</a></li>
+                    <li><a class="text-muted" href="#">adresa</a></li>
+                    <li><a class="text-muted" href="#">úradné hodiny</a></li>
+                    <li><a class="text-muted" href="#">bankové spojenie</a></li>
+                </ul>
+                <h6 class="text-uppercase">Informácie pre verejnosť</h6>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Zoznam vyvezených liekov</a></li>
+                    <li><a class="text-muted" href="#">MZ SR</a></li>
+                    <li><a class="text-muted" href="#">Národný portál zdravia</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h6 class="text-uppercase">O nás</h6>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Dotazníky</a></li>
+                    <li><a class="text-muted" href="#">Hlavní predstavitelia</a></li>
+                    <li><a class="text-muted" href="#">Základné dokumenty</a></li>
+                    <li><a class="text-muted" href="#">Zmluvy za ŠÚKL</a></li>
+                    <li><a class="text-muted" href="#">História a súčasnosť</a></li>
+                    <li><a class="text-muted" href="#">Národná spolupráca</a></li>
+                    <li><a class="text-muted" href="#">Medzinárodná spolupráca</a></li>
+                    <li><a class="text-muted" href="#">Poradové orgány</a></li>
+                    <li><a class="text-muted" href="#">Legislatíva</a></li>
+                    <li><a class="text-muted" href="#">Priestupky a iné správne konanie</a></li>
+                    <li><a class="text-muted" href="#">Zoznam dlžníkov</a></li>
+                    <li><a class="text-muted" href="#">Sadzobník ŠÚKL</a></li>
+                    <li><a class="text-muted" href="#">Verejné obstarávanie</a></li>
+                    <li><a class="text-muted" href="#">Vzdelávacie akcie a prezentácie</a></li>
+                    <li><a class="text-muted" href="#">Konzultácie</a></li>
+                    <li><a class="text-muted" href="#">Voľné pracovné miesta (0)</a></li>
+                    <li><a class="text-muted" href="#">Poskytovanie informácií</a></li>
+                    <li><a class="text-muted" href="#">Sťažnosti a petície</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h6 class="text-uppercase">Médiá</h6>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Tlačové správy</a></li>
+                    <li><a class="text-muted" href="#">Lieky v médiách</a></li>
+                    <li><a class="text-muted" href="#">Kontakt pre médiá</a></li>
+                </ul>
+                <h6 class="text-uppercase">Databázy a servis</h6>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Databáza liekov a zdravotníckych pomôcok</a></li>
+                    <li><a class="text-muted" href="#">Iné zoznamy</a></li>
+                    <li><a class="text-muted" href="#">Kontaktný formulár</a></li>
+                    <li><a class="text-muted" href="#">Mapa stránok</a></li>
+                    <li><a class="text-muted" href="#">A - Z index</a></li>
+                    <li><a class="text-muted" href="#">Linky</a></li>
+                    <li><a class="text-muted" href="#">RSS</a></li>
+                    <li><a class="text-muted" href="#">Doplnok pre internetový prehliadač</a></li>
+                    <li><a class="text-muted" href="#">Prehliadače formátov</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h6 class="text-uppercase">Drogové prekurzory</h6>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Aktuality</a></li>
+                    <li><a class="text-muted" href="#">Legislatíva</a></li>
+                    <li><a class="text-muted" href="#">Pokyny</a></li>
+                    <li><a class="text-muted" href="#">Kontakt</a></li>
+                </ul>
+                <h6 class="text-uppercase">Iné</h6>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Linky</a></li>
+                    <li><a class="text-muted" href="#">Mapa stránok</a></li>
+                    <li><a class="text-muted" href="#">FAQ</a></li>
+                    <li><a class="text-muted" href="#">Podmienky používania</a></li>
+                </ul>
+                <h6 class="text-uppercase text-primary">Rapid alert systém</h6>
+                <ul class="list-unstyled text-small text-primary">
+                    <li><a href="#">Rýchla výstraha vyplývajúca z nedostatkov v kvalite liekov</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</div>
